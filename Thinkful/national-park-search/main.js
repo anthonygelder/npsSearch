a1372c78ac8f8c1a5d130f4e1a9e68c0e6a3881b
Anthony Gelder <anthony.gelder@gmail.com>
2019-08-03 12:16:10 -0700

    working

--- Thinkful/national-park-search/main.js
+++ Thinkful/national-park-search/main.js
@@ -1,37 +1,43 @@
-const apiKey = 'dtVNYedoCTKnifpqrvqfsahKihMpkNYvLOa5X3bn'
-const url = 'https://developer.nps.gov/api/v1/parks?stateCode=WA&limit=10&api_key=dtVNYedoCTKnifpqrvqfsahKihMpkNYvLOa5X3bn'
+// const apiKey = 'dtVNYedoCTKnifpqrvqfsahKihMpkNYvLOa5X3bn'
 
-function getUrl(search, limit, url) {
-    
-}
-
-function getPark(url) {
-    fetch(url).then(response => response.json()).then(responseJson =>
-        console.log(responseJson));
-}
-
-// fetch(url)
-//     .then(response => {
-//         if (response.ok) {
-//             return response.json()
-//         }
-//         throw new Error(response.statusText)
-//     })
-//     .then(responseJson => displayResults(responseJson))
-//     .catch(err => {
-//         $('#js-error-message').text(`Something went wrong: ${err.message}`)
-//     })
-// }
-
-function main() {
-    $('.hearMe').on('click', function () {
-        event.preventDefault();
-        const searchTerm = $('#js-search-term').val();
-        const maxResults = $('#js-max-results').val();
-        $('.js-search-term').val('');
-        getPark(searchTerm, maxResults)
-    })
-}
-
-
-$(main)
+function generateHtml(obj) {
+    return `
+      <div class="park">
+        <p>${obj.name}</p>
+        <p>${obj.description}</p>
+        <a href="${obj.url}">URL</a>
+      </div>
+    `
+  }
+  
+  function processParkData(responseData) {
+    console.log(responseData.data[0].description)
+    const parkDataHTML = responseData.data.map(function(obj) {
+      return generateHtml(obj)
+    }).join('')
+  
+    // render
+    $('.display').html(parkDataHTML);
+  }
+  
+  function getPark(search, limit) {
+      const apiUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${search}&limit=${limit}&api_key=dtVNYedoCTKnifpqrvqfsahKihMpkNYvLOa5X3bn`
+  
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(responseJson => processParkData(responseJson));
+  }
+  
+  function main() {
+      $('form').on('submit', function(event) {
+  
+          event.preventDefault();
+          const searchTerm = $(event.currentTarget).find('#js-search-term').val();
+          const maxResults = $(event.currentTarget).find('#js-max-results').val();
+          $('.js-search-term').val('');
+          getPark(searchTerm, maxResults)
+      })
+  }
+  
+  
+  $(main)
